b084d9c910888938b2b0a80a92b4396cfb4910f2
Sergey <sergejananev78@gmail.com>
2024-06-20 17:36:38 +0300

    Levenstein.cpp

--- Levenstein.cpp
+++ Levenstein.cpp
@@ -4,7 +4,9 @@
 #include <vector>
 
 
+// функция для полчения значения в ячейке [i][j]
 int getElement (std::string& a, std::string& b, int i, int j, std::vector<std::vector<int>>& D) {
+    // все по алгоритму из методички
     if ((i == 0) && (j == 0))
         return 0;
 
@@ -21,23 +23,27 @@ int getElement (std::string& a, std::string& b, int i, int j, std::vector<std::v
 }
 
 int LevensteinDistance (std::string& a, std::string& b) {
+    // создадим таблицу
     std::vector<std::vector<int>> D (
     a.length () + 1, std::vector<int> (b.length () + 1, 0));
 
+    // пройдемся по таблице и заполним ее
     for (int i = 0; i < a.length () + 1; ++i) {
         for (int j = 0; j < b.length () + 1; ++j)
             D[i][j] = getElement (a, b, i, j, D);
     }
 
+
+    // вернем значение в последней ячейке
     return D[a.length ()][b.length ()];
 }
 
-int main () {
+int main () { // функция для проверки
 
-    std::string a = "avstria";
-    std::string b = "avstralia";
+    std::string a = "labrador"; // создадим две строки (на русском не пашет, хз почему)
+    std::string b = "gibraltar";
 
-    std::cout << LevensteinDistance (b, a) << std::endl;
+    std::cout << LevensteinDistance (b, a) << std::endl; // выведем результат
 
 
     return 0;
